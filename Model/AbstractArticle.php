6a85939d0fa1dd9e9b6cf862464294b3cc2c4a1b
Quentin SOMAZZI <qsomazzi@users.noreply.github.com>
2017-01-31 12:06:54 +0100

    Fix setTags type on article setter (#31)

--- Model/AbstractArticle.php
+++ Model/AbstractArticle.php
@@ -84,7 +84,7 @@ abstract class AbstractArticle implements ArticleInterface
     protected $categories;
 
     /**
-     * @var Collection
+     * @var ArrayCollection
      */
     protected $tags;
 
@@ -350,7 +350,7 @@ abstract class AbstractArticle implements ArticleInterface
     /**
      * {@inheritdoc}
      */
-    public function setTags(Collection $tags = null)
+    public function setTags(array $tags = null)
     {
         $this->tags = $tags;
 
